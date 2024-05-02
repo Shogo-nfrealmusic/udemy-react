ae115df7f5094b235d75d61b5877048b60a341c7
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-02 20:07:33 +0900

    fragment

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,22 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum, dolor sit amet 
+        consectetur adipisicing elit. 
+        Veniam necessitatibus quos ab deserunt nihil.
+        Nisi distinctio commodi iste tempora dolor 
+        rerum facere ad molestiae earum, fugit 
+        consequatur quos eos reprehenderit.
+      </p>
+    </React.Fragment>
+
   );
 };
 
