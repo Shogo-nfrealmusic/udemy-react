bc006194a097cfddfa9a96f1501991e5b9a00478
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-03 14:28:43 +0900

    children

--- 04_react_basic/src/110_props_children/start/Example.jsx
+++ 04_react_basic/src/110_props_children/start/Example.jsx
@@ -2,14 +2,26 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
+/* POINT コンポーネントの子要素は props.children に渡る！
+コンポーネントが子要素を持つ場合、props.children という特別なプロパティとして受け渡されます。
+ */
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="順番通り渡す">
+        <Profile key={profile[0].name} {...profile[0]} />
+        <Profile key={profile[1].name} {...profile[1]} />
+      </Container>
+
+      <Container
+        title="個別渡す"
+        first={<Profile key={profile[0].name} {...profile[1]} />}
+        second={<Profile key={profile[1].name} {...profile[0]} />}
+      />
     </div>
   );
 };
