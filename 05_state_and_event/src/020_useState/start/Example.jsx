c254f2310ae2d9b47252c03a2c1b6ee3d22792ce
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-03 15:27:15 +0900

    useState

--- 05_state_and_event/src/020_useState/start/Example.jsx
+++ 05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,8 +1,15 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  let [val, setVal] = useState();
   return (
     <>
-      
+      <input 
+      type="text"
+      onChange={(e) => {
+        // const setFn = setVal;
+        setVal(e.target.value);
+        }} /> = {val}
     </>
   );
 };
