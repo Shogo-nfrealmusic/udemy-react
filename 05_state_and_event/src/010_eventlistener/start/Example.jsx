8e11a3c159a78e74e3d6a58e3ba1cdd0010388c7
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-03 15:07:53 +0900

    event

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,15 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('click');
+  }
+  const clickHandler2 = () => {
+    alert('billionaire');
+  }
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>Click!</button>
+      <button onClick={clickHandler2}>Click!</button>
+    </>
   );
 };
 
