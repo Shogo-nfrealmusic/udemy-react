95b93cae1f78632e3fed0297dda206aaa883f1ac
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-07 19:09:55 +0900

    list and form

--- 06_control_and_form/src/130_reminder/start/components/Form.jsx
+++ 06_control_and_form/src/130_reminder/start/components/Form.jsx
@@ -3,20 +3,23 @@ import { useState } from 'react';
 function Form({createTodo}) {
     const [enteredTodo, setEnteredTodo ] = useState("");
     const addTodo = (e) => {
-        
+        e.preventDefault();
         const newTodo = {
             id: Math.floor(Math.random() * 1e5),
             content: enteredTodo,
         };
         createTodo(newTodo);
+        setEnteredTodo("");
     }
   return (
     <div>
+        <form onSubmit={addTodo}>
         <input type="text" 
         value={enteredTodo}
         onChange={(e) => setEnteredTodo(e.target.value)}/>
-        <button onClick={addTodo}>追加</button>
+        <button>追加</button>
         
+        </form>
     </div>
     
     
