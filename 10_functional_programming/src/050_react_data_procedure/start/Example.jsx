598969036ba31d958a5ff333f506bbca1ef944aa
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-16 16:42:08 +0900

    date procedure

--- 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+// /* 
+/* 
+POINT Reactでの状態管理(state)と処理(関数コンポーネント)の分離
+
+関数型（純粋関数）
+・fn(決まった引数) : propsとstateに依存
+  -> 決まった戻り値 : JSX
+*/
+// // ・関数外の状態（データ）は参照・変更しない。
+// 関数外に影響を及ぼさない。
+
 const Example = () => {
   const [ state, setState ] = useState(0);
   const increment = () => {
