a8d5ed6b04bf390a6e3c3b9b6bdf66a5e4d23a4d
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-27 12:39:16 +0900

    useEffect dependent"

--- 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
@@ -7,6 +7,10 @@ const Example = () => {
       setTime(prev => prev + 1);
     }, 1000);
   }, []);
+  useEffect(() => {
+    document.title = 'counter:' + time;
+    window.localStorage.setItem('time-key', time);
+  }, [time]);
   return (
     <>
     <h3>
