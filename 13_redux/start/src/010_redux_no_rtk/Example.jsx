bd43905f6b5546ed008893c9be7a2504d6cf5892
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-05-31 17:46:47 +0900

    redux

--- 13_redux/start/src/010_redux_no_rtk/Example.jsx
+++ 13_redux/start/src/010_redux_no_rtk/Example.jsx
@@ -1,10 +1,11 @@
 import Counter from "./components/Counter";
 import { CounterProvider } from "./context/CounterContext";
 import { Provider } from "react-redux";
+import  store  from "./store";
 
 const Example = () => {
   return (
-    <Provider>
+    <Provider store={store}>
       <Counter />
     </Provider>
   );
