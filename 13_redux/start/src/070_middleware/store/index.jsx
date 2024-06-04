a190fb3719e2b5b832bf1c77458a235722192675
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-04 16:04:26 +0900

    middleware

--- 13_redux/start/src/070_middleware/store/index.jsx
+++ 13_redux/start/src/070_middleware/store/index.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./modules/counter";
-
+import logger from "./middleware/logger";
 export default configureStore({
   reducer: {
     counter: reducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().
+  concat(logger)
+    
+  
 });
---
+++ 13_redux/start/src/070_middleware/store/middleware/logger.jsx
@@ -0,0 +1,10 @@
+const logger = (store) => {
+    return (next) => {
+      return (action) => {
+        console.log(action, store.getState())
+        next(action);
+        console.log(action, store.getState())
+      };
+    }
+  }
+  export default logger;
