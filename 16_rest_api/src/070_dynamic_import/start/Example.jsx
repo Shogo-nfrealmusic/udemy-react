c0aec2081e044c0b1f05a83c073d310747c4dcb2
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-08 16:59:42 +0900

    dynamic import

--- 16_rest_api/src/070_dynamic_import/start/Example.jsx
+++ 16_rest_api/src/070_dynamic_import/start/Example.jsx
@@ -1,5 +1,9 @@
+import { add } from "./add"
 const Example = () => {
-  
+    import("./add").then(module => {
+        console.log(module);
+    })
+//   console.log(add(1,2));
 };
 
 export default Example;
