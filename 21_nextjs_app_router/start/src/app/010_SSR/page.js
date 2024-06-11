b86c6fd158e462d551b6eaa91f9d991837d6ecef
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-11 13:01:38 +0900

    use client

--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,5 +1,17 @@
+"use client"
+
+import { useEffect, useState } from "react"
 
 
 export default function SSR() {
-    return <div>SSR Page</div>
+    const [ state, setState ] = useState(undefined);
+    useEffect(() => {
+        setState('client Loaded');
+    }, [])
+    return (
+    <>
+        <div>SSR Page</div>
+        <div>{state}</div>
+    </>
+    )
 }
