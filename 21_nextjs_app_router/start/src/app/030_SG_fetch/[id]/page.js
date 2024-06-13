9e95d2ebd4bebd548e62b4ebba50f629f73c842f
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-13 17:05:20 +0900

    test SG

--- 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ 21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,6 +1,12 @@
 import { ENDPOINT } from "@/constants";
 import Article from "../../../components/article";
 
+
+export async function generateStaticParams() {
+  const data = await fetch(ENDPOINT).then(res => res.json());
+  console.log(data);
+  return data.map(record => ({ id: record.id }))
+}
 export default async function Detail({ params }) {
   const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
     res.json()
