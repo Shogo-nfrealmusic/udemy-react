c568076b564ca5b5aedda47ebbb026e4aa6203df
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-16 15:44:41 +0900

    get element

---
+++ 19_test/src/020_get_element/start/components/Greet.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+it('should have h1 tag', () => {
+    const {debug, container} = render(<Greet />);
+    // const h1El = screen.getByText("こんにちは");
+    // expect(h1El).toBeInTheDocument();
+
+    // const radioEl = screen.getByRole('radio')
+    // debug(radioEl);
+    // expect(radioEl).toBeInTheDocument();
+
+    // const imgEl = screen.getByRole('img')
+    // debug(imgEl);
+
+    // const headingEl = screen.getByRole('heading', { name: 'こんにちは'})
+    const h2El = container.querySelector('h2');
+    debug(h2El);
+})
