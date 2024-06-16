28024038a03db5cc8084a3346967106176a2d8fc
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-16 16:02:50 +0900

    how to test

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -3,4 +3,18 @@ Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
+test('カウントアップボタンで結果が+1されるか', () => {
+    // Arrage
+    render(<Counter originCount={0} />);
+
+    // Act
+    const btn = screen.getByRole('button', {name: 'カウントアップ'});
+    fireEvent.click(btn)
+
+    // Assertion
+    const spanEl = screen.getByText('現在のカウント:1')
+    expect(spanEl).toBeInTheDocument();
+});
